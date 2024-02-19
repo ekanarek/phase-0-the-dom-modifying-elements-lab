f35ee016e51d568304549dbba29e74e8207f83b5
Emily Kanarek <emily.kanarek@powerhrg.com>
2024-02-18 19:50:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const body = document.querySelector("body");
+const main = body.querySelector("main");
+body.removeChild(main);
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "Emily is the champion";
